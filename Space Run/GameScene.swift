5eb2938f277fc53549a478e393fc749f267e1a9d
Jack Hall <hall.d.jack@gmail.com>
2015-11-24 15:11:28 -0600

    - Add automatic photon torpedoes. - Refactor out some constants for ship speed, torpedo speed and torpedo interval.

--- Space Run/GameScene.swift
+++ Space Run/GameScene.swift
@@ -10,7 +10,12 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    let shipSpeed: CGFloat = 5.0
+    let torpedoFlightDuration: NSTimeInterval = 5.0
+    let torpedoInterval = 7.0
+    
     var lastUpdatedTime: NSTimeInterval = 0.0
+    var lastShotFiredTime: NSTimeInterval = 0.0
     weak var shipTouch: UITouch?
     var ship: SKSpriteNode? = nil
     
@@ -37,19 +42,31 @@ class GameScene: SKScene {
         
         if ((shipTouch) != nil) {
             _moveTowardPoint((shipTouch?.locationInNode(self))!, timeDelta: delta)
-            //          self.ship?.position = self.shipTouch!.locationInNode(self)
+            if (currentTime - lastShotFiredTime > torpedoInterval) {
+                _fireZeMissles()
+                lastShotFiredTime = currentTime
+            }
         }
         
         lastUpdatedTime = currentTime;
     }
     
+    func _fireZeMissles() {
+        let torpedo = SKSpriteNode(imageNamed: "photon")
+        torpedo.position = (ship?.position)!
+        self.addChild(torpedo)
+        
+        let fly = SKAction.moveByX(0, y: self.size.height + torpedo.size.height, duration: torpedoFlightDuration)
+        
+        torpedo.runAction(fly)
+    }
+    
     func _moveTowardPoint(point: CGPoint, timeDelta: NSTimeInterval) {
-        speed = 5
         if (ship != nil) {
             let distanceRemaining = sqrt(pow(ship!.position.x - point.x, 2) + pow(ship!.position.y - point.y, 2))
             
             if (distanceRemaining > 4) {
-                let distanceToTravel = distanceRemaining / speed
+                let distanceToTravel = distanceRemaining / shipSpeed
                 let angle = atan2(point.y - ship!.position.y, point.x - ship!.position.x)
                 let yOffset = distanceToTravel * sin(angle)
                 let xOffset = distanceToTravel * cos(angle)
